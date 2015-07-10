12281504289323e0ee9f64e487ca1c17b2122a5d
kent10ou <kent10ou@gmail.com>
2015-07-10 16:34:18 -0700

    created moving enemies, player, and background

--- client/watchout.js
+++ client/watchout.js
@@ -16,8 +16,9 @@ var gameStats = {
 var gameBoard = d3.select('.gameboard').append('svg:svg')
 	.attr('width', gameOptions.width)
 	.attr('height', gameOptions.height)
-	.style({'border': 'solid 1px black'})
+	.style({'border': 'solid 1px black', 'background-color': 'blue'})
 
+// creating x and y plane on gameBoard
 var axes = {
 	x: d3.scale.linear().domain([0,100]).range([0, gameOptions.width]),
 	y: d3.scale.linear().domain([0,100]).range([0, gameOptions.height])
@@ -32,18 +33,47 @@ var createEnemies = function () {
 			id: i
 		})
 	}
-	return results
+	return results;
 }
 
-var Enemies = createEnemies();
+var enemies = createEnemies();
 
-gameBoard.selectAll('rect').data(Enemies)
-	.enter().append('svg:rect')
+// place enemies on gameBoard
+gameBoard.selectAll('rect').data(enemies)
+	.enter().append('rect')
 	.attr('width', 10)
 	.attr('height', 10)
 	.attr('x', function (d) { return axes.x(d.x) })
 	.attr('y', function (d) { return axes.y(d.y) })
-	
+	.style({'fill': 'yellow'})
+
+var createPlayer = function () {
+	var playerArr = [];
+	playerArr.push({
+		x: 50,
+		y: 50,
+		id: "player"
+	})
+	return playerArr;
+}
+
+// place player on gameBoard
+var player = createPlayer();
+
+gameBoard.selectAll('circle').data(player)
+	.enter().append('circle')
+	.attr('cx', function (d) { return axes.x(d.x)})
+	.attr('cy', function (d) { return axes.y(d.y)})
+	.attr('r', 5)
 
 
+
+setInterval(function () {
+	var newEnemiesPos = createEnemies();
+	gameBoard.selectAll('rect').data(newEnemiesPos)
+		.transition()
+		.attr('x', function (d) { return axes.x(d.x) })
+		.attr('y', function (d) { return axes.y(d.y) })
+		.duration(2000)
+}, 2000)
 
